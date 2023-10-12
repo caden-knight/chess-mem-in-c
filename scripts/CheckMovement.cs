af2606684b7c70865776bdb9daa3ef18c645ae08
Caden Morrison <cadenknight@gmail.com>
2023-10-12 17:06:48 -0600

    progress in determining knight movement

--- scripts/CheckMovement.cs
+++ scripts/CheckMovement.cs
@@ -3,105 +3,151 @@ using System;
 
 public partial class CheckMovement : Control
 {
-    public Node2D chessBoard;
+	public Node2D chessBoard;
 
-    public bool squareOccupied = true;
-    public string pieceOnSquare;
-    public string squareCoord;
+	public bool squareOccupied;
+	public string pieceOnSquare;
+	public string squareCoord;
 
-    private bool mouseClicked = false;
-    private bool highlighted = false;
-    private Vector2 highlightScale = new Vector2(0.03f, 0.03f);
-    private Color originalColor;
+	private bool mouseClicked = false;
+	private bool highlighted = false;
+	private Vector2 highlightScale = new Vector2(0.03f, 0.03f);
+	private Vector2 normalAnimScale = new Vector2(0.412f, 0.411f);
+	private string[] coordLetters = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
-    public override void _Ready()
-    {
-        originalColor = GetChild<ColorRect>(0).Color;
-        chessBoard = GetNode<Node2D>("/root/ChessMemory/ChessBoard");
 
-    }
+	public override void _Ready()
+	{
+		chessBoard = GetNode<Node2D>("/root/ChessMemory/ChessBoard");
+	}
 
-    public override void _Input(InputEvent @event)
-    {
+	public override void _Input(InputEvent @event)
+	{
 
-        if (@event is InputEventMouseButton mouseButton)
-        {
-            // check to see if player left clicked
-            if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
-            {
-                // get mouse position of where the player clicked
-                Vector2 mousePosition = GetGlobalMousePosition();
-
-                // check to see if player clicked somewhere on the chessboard
-                // REMEMBER THIS CODE!
-                if (GetGlobalRect().HasPoint(mousePosition))
-                {
-                    ClearHighlightedSquares(chessBoard);
-
-                    AnimatedSprite2D squareAnimation = GetChild<AnimatedSprite2D>(2);
-
-                    if (squareAnimation.Animation == "w-P")
-                    {
-                        squareCoord = GetChild<Label>(1).Text;
-
-                        GD.Print(squareOccupied);
-                        GD.Print(pieceOnSquare);
-
-                        // FIXME: potential problem if planning on using squareOccupied in if statement above
-                        squareOccupied = false;
-                        // squareAnimation.Visible = squareOccupied;
-
-                        Godot.Collections.Array<Node> squares = GetNode("/root/ChessMemory/ChessBoard").GetChildren();
-
-                        foreach (Control square in squares)
-                        {
-                            //TODO: come up with better variable names. they're getting confusing.
-                            string squareLabel = square.GetChild<Label>(1).Text;
-                            int rankNum = Convert.ToInt32(squareCoord[1].ToString()) + 1;
-                            int rankNumOption2 = Convert.ToInt32(squareCoord[1].ToString()) + 2;
-                            string destinationCoord1 = $"{squareCoord[0]}{rankNum}";
-                            string destinationCoord2 = $"{squareCoord[0]}{rankNumOption2}";
-
-                            if (squareLabel == destinationCoord1 || squareLabel == destinationCoord2)
-                            {
-                                AnimatedSprite2D squareSprite = square.GetChild<AnimatedSprite2D>(2);
-                                squareSprite.Animation = "highlight";
-                                squareSprite.Visible = true;
+		if (@event is InputEventMouseButton mouseButton)
+		{
+			// check to see if player left clicked
+			if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
+			{
+				// get mouse position of where the player clicked
+				Vector2 mousePosition = GetGlobalMousePosition();
 
 
 
-                                squareSprite.Scale = highlightScale;
-                            }
+				// check to see if player clicked somewhere on the chessboard
+				// REMEMBER THIS CODE!
+				if (GetGlobalRect().HasPoint(mousePosition))
+				{
+					GD.Print(squareOccupied);
+					ClearHighlightedSquares(chessBoard);
 
-                            // if (squareLabel == squareCoord)
-                            // {
-                            // 	GD.Print("destination");
-                            // 	ColorRect squareDest = (ColorRect)square;
+					AnimatedSprite2D squareAnimation = GetChild<AnimatedSprite2D>(2);
 
-                            // 	AnimatedSprite2D squareDestAnimation = squareDest.GetChild(0).GetChild<AnimatedSprite2D>(1);
+					HighlightPossibleMoves(squareAnimation.Animation);
+				}
+			}
+		}
+	}
+	private void ClearHighlightedSquares(Node2D chessBoard)
+	{
+		Godot.Collections.Array<Node> squares = chessBoard.GetChildren();
 
-                            // 	squareDestAnimation.Animation = "w-P";
-                            // }
+		foreach (Control square in squares)
+		{
+			AnimatedSprite2D squareAnim = square.GetChild<AnimatedSprite2D>(2);
+			// GD.Print("caden");
 
-                        }
-                    }
-                }
-            }
-        }
-    }
-    private void ClearHighlightedSquares(Node2D chessBoard)
-    {
-        Godot.Collections.Array<Node> squares = chessBoard.GetChildren();
+			if (squareAnim.Animation == "highlight")
+			{
+				squareAnim.Visible = false;
+			}
+		}
+	}
 
-        foreach (Control square in squares)
-        {
-            AnimatedSprite2D squareAnim = square.GetChild<AnimatedSprite2D>(2);
-            // GD.Print("caden");
+	private void HighlightPossibleMoves(string piece)
+	{
+		Godot.Collections.Array<Node> squares = GetNode("/root/ChessMemory/ChessBoard").GetChildren();
 
-            if (squareAnim.Animation == "highlight")
-            {
-                squareAnim.Visible = false;
-            }
-        }
-    }
+		switch (piece)
+		{
+			case "w-P":
+				squareCoord = GetChild<Label>(1).Text;
+
+				GD.Print(squareOccupied);
+				GD.Print(pieceOnSquare);
+
+				// FIXME: potential problem if planning on using squareOccupied in if statement above
+				// squareOccupied = false;
+				// squareAnimation.Visible = squareOccupied;
+
+				foreach (Control square in squares)
+				{
+					//TODO: come up with better variable names. they're getting confusing.
+					string squareLabel = square.GetChild<Label>(1).Text;
+					int rankNum = Convert.ToInt32(squareCoord[1].ToString()) + 1;
+					int rankNumOption2 = Convert.ToInt32(squareCoord[1].ToString()) + 2;
+					string destinationCoord1 = $"{squareCoord[0]}{rankNum}";
+					string destinationCoord2 = $"{squareCoord[0]}{rankNumOption2}";
+
+					if (squareLabel == destinationCoord1 || squareLabel == destinationCoord2)
+					{
+						GD.Print($"CM: {((CheckMovement)square).squareOccupied}");
+						if (((CheckMovement)square).squareOccupied == false)
+						{
+							// Highlight move squares and adjust the highlight's sprite size
+							AnimatedSprite2D squareSprite = square.GetChild<AnimatedSprite2D>(2);
+							squareSprite.Animation = "highlight";
+							squareSprite.Visible = true; // ensure it's visible
+							squareSprite.Scale = highlightScale;
+						}
+
+					}
+				}
+				break;
+
+			case "w-N":
+				squareCoord = GetChild<Label>(1).Text;
+
+				GD.Print(squareOccupied);
+				GD.Print(pieceOnSquare);
+
+				// FIXME: potential problem if planning on using squareOccupied in if statement above
+				squareOccupied = false;
+				// squareAnimation.Visible = squareOccupied;
+
+
+				foreach (Control square in squares)
+				{
+					DetermineKnightMoves(square);
+				}
+				break;
+
+		}
+	}
+
+	// TODO: complete logic for knight moves
+	private void DetermineKnightMoves(Control square)
+	{
+		// variables for calculating and identifying the coordinate
+		string squareLabel = square.GetChild<Label>(1).Text;
+		int rankNum = Convert.ToInt32(squareCoord[1].ToString()) + 2;
+		int rankNumOption2 = Convert.ToInt32(squareCoord[1].ToString()) - 2;
+		string destinationCoord1 = $"{squareCoord[0]}{rankNum}";
+		string destinationCoord2 = $"{squareCoord[0]}{rankNumOption2}";
+
+		// ensures the square isn't occupied
+		// so the game won't highlight a spot that is occupied by another piece
+		if (!((CheckMovement)square).squareOccupied)
+		{
+			if (squareLabel == destinationCoord1 || squareLabel == destinationCoord2)
+			{
+				// Highlight move squares and adjust the highlight's sprite size
+				AnimatedSprite2D squareSprite = square.GetChild<AnimatedSprite2D>(2);
+				squareSprite.Animation = "highlight";
+				squareSprite.Visible = true; // make it visible
+				squareSprite.Scale = highlightScale;
+			}
+
+		}
+	}
 }
